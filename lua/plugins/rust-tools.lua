2606abb6dd32741246c4a2c92b78b2992ab528be
Cory Forsstrom <cory.forsstrom@lich.io>
2022-03-16 15:02:12 -0700

    Use default inlay hint setup

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -6,9 +6,6 @@ local opts = {
             use_telescope = true
         },
         inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
         },
     },
 
